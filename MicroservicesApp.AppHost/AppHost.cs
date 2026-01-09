fe313ed23d94028c58e96e310ae338af264eeadf
Bohan Feng <fengb3@gmail.com>
2026-01-10 00:35:26 +0800

    Add Postgres support to AppHost
    Introduced a Postgres container and database to the distributed application setup. Updated web project and example API service to reference the new database. Added Aspire.Hosting.PostgreSQL package to project dependencies.

--- MicroservicesApp.AppHost/AppHost.cs
+++ MicroservicesApp.AppHost/AppHost.cs
@@ -4,20 +4,26 @@ var builder = DistributedApplication.CreateBuilder(args);
 // Add Redis container
 var redis = builder.AddRedis("redis").WithRedisInsight();
 
+// Add Postgres container (+ a named database)
+var db = builder.AddPostgres("postgres").AddDatabase("db");
+
 var web = builder.AddProject<Projects.JmHell>("web")
     .WithHttpHealthCheck("/health")
     .WithEnvironment("CONSUMER_GROUP", "a-consumers")
     .WithReference(redis)
-    .WaitFor(redis);
-;
+    .WithReference(db, connectionName: "db")
+    .WaitFor(redis)
+    .WaitFor(db);
 
-// Add API Service with Redis reference
+// Add API Service with Redis/Postgres reference
 // var apiService = builder
 //     .AddProject<Projects.MicroservicesApp_ApiService>("apiservice")
 //     .WithHttpHealthCheck("/health")
 //     .WithEnvironment("CONSUMER_GROUP", "a-consumers")
 //     .WithReference(redis)
-//     .WaitFor(redis);
+//     .WithReference(db)
+//     .WaitFor(redis)
+//     .WaitFor(db);
 
 // Add Python worker using Aspire Python hosting integration
 var pythonWorker = builder
@@ -25,7 +31,9 @@ var pythonWorker = builder
     .WithUv()
     .WithEnvironment("CONSUMER_GROUP", "a-consumers")
     .WithReference(redis)
-    .WaitFor(redis);
+    // .WithReference(db, connectionName: "db") // we dont need db for python worker
+    .WaitFor(redis)
+    .WaitFor(db);
 
 
 builder.Build().Run();
